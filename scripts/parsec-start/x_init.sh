ed668d14cfe9f04ed87e115bd7260ce0c18bb65d
liberavia <andre.gregor.herrmann@gmail.com>
2018-10-07 05:09:19 +0200

    Debugged into working state

--- scripts/parsec-start/x_init.sh
+++ scripts/parsec-start/x_init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sleep 2
-sudo fbi -T 7 -d /dev/fb0 --noverbose /home/osmc/.kodi/addons/plugin.program.x11-launcher/fanart.jpg &
+sudo fbi -T 7 -d /dev/fb0 --noverbose /home/osmc/.kodi/addons/plugin.program.parsec-launcher/fanart.jpg &
 sleep 3
+sudo openvt -c 7 -s -f clear
 sudo su osmc -c "startx /home/osmc/Scripts/parsec-start/start_scc_parsec `echo $@`"
 sudo openvt -c 7 -s -f clear
 sudo systemctl start mediacenter
