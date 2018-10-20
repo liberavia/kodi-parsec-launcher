949ee1a69bc8da6796201d973415ae32503267b6
liberavia <andre.gregor.herrmann@gmail.com>
2018-10-20 14:51:15 +0200

    Adding context menu actions, added computer and user information

--- addon/plugin.program.parsec-launcher/addon.py
+++ addon/plugin.program.parsec-launcher/addon.py
@@ -8,48 +8,73 @@ import sys
 import os
 import urllib
 import urllib2
+import requests
 import json
 import xbmc
 import xbmcgui
 import xbmcaddon
 import plugintools
+import pprint
 
 # plugin constants
 __plugin__ = "Parsec"
 __author__ = "liberavia"
 __url__ = "https://github.com/liberavia/kodi-parsec"
 __git_url__ = "https://github.com/liberavia/kodi-parsec"
-__credits__ = "mcobit"
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 API_BASEURL = "https://parsecgaming.com/v1/"
 API_AUTHURL = API_BASEURL + "auth"
 API_LIST_COMPUTERS = API_BASEURL + "server-list?include_managed=true"
+API_USER_INFO = API_BASEURL + "me"
 API_SWITCH_COMPUTER_ON = API_BASEURL + "activate-lease"
 API_SWITCH_COMPUTER_OFF = API_BASEURL + "deactivate-lease"
 
-THUMBNAIL_PATH = os.path.join(plugintools.get_runtime_path() ,"resources" , "img")
+ADDON_BASE_PATH = os.path.dirname(__file__)
+ADDON_SCRIPT_PATH = os.path.join(ADDON_BASE_PATH, 'scripts')
+THUMBNAIL_PATH = os.path.join(plugintools.get_runtime_path(), "resources", "img")
 DEFAULT_FANART = os.path.join(plugintools.get_runtime_path(), "fanart.jpg")
-DEFAULT_LOGO = os.path.join(THUMBNAIL_PATH,"parsec-logo.png")
+DEFAULT_LOGO = os.path.join(THUMBNAIL_PATH, "parsec-logo.png")
 LOGO_SERVER_ON = DEFAULT_LOGO
 LOGO_SERVER_OFF = DEFAULT_LOGO
 LOGO_SERVER_PENDING = DEFAULT_LOGO
+LOGO_SERVER_CONNECT = DEFAULT_LOGO
 
 parsec_session_id = False
 current_computer = False
 dialog = xbmcgui.Dialog()
 addon = xbmcaddon.Addon(id='plugin.program.parsec-launcher')
+user_agent = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:62.0) Gecko/20100101 Firefox/62.0'
 
 # MAIN MENU
-LANG_TITLE_LAUNCH_PARSEC=addon.getLocalizedString(30020).encode('utf-8')
+LANG_TITLE_CONNECT_PARSEC=addon.getLocalizedString(30020).encode('utf-8')
 LANG_TITLE_MANAGE_COMPUTERS=addon.getLocalizedString(30021).encode('utf-8')
 LANG_TITLE_MANAGE=addon.getLocalizedString(30022).encode('utf-8')
 LANG_TITLE_MANAGE_COMPUTER_SWITCH_ON=addon.getLocalizedString(30023).encode('utf-8')
 LANG_TITLE_MANAGE_COMPUTER_SWITCH_OFF=addon.getLocalizedString(30024).encode('utf-8')
 LANG_TITLE_MANAGE_COMPUTER_IS_PENDING=addon.getLocalizedString(30025).encode('utf-8')
+LANG_PARSEC=addon.getLocalizedString(30026).encode('utf-8')
+LANG_TITLE_STATUS=addon.getLocalizedString(30027).encode('utf-8')
+LANG_TITLE_CREATED=addon.getLocalizedString(30028).encode('utf-8')
+LANG_TITLE_LAST_UPDATED=addon.getLocalizedString(30029).encode('utf-8')
+LANG_TITLE_PROVIDER=addon.getLocalizedString(30030).encode('utf-8')
+LANG_TITLE_MACHINE_TYPE=addon.getLocalizedString(30031).encode('utf-8')
+LANG_TITLE_REGION=addon.getLocalizedString(30032).encode('utf-8')
+LANG_TITLE_CREDIT=addon.getLocalizedString(30033).encode('utf-8')
+LANG_TITLE_PLAYTIME=addon.getLocalizedString(30034).encode('utf-8')
+
+
 LANG_TITLE_MANAGE_COMPUTER_MESSAGE_PENDING=addon.getLocalizedString(30200).encode('utf-8')
 LANG_TITLE_MANAGE_COMPUTER_MESSAGE_ON=addon.getLocalizedString(30201).encode('utf-8')
 LANG_TITLE_MANAGE_COMPUTER_MESSAGE_OFF=addon.getLocalizedString(30202).encode('utf-8')
+
+
+def trigger_notification(message, time=5000):
+    # Trigger notification with given message for given time in milliseconds
+    __addonname__ = addon.getAddonInfo('name')
+    __icon__ = addon.getAddonInfo('icon')
+
+    xbmc.executebuiltin('Notification(%s, %s, %d, %s)' % (__addonname__, message, time, __icon__))
 
 
 # Entry point
@@ -69,19 +94,98 @@ def run():
 
 def main_list(params):
     # Main menu
-    plugintools.set_view(plugintools.THUMBNAIL)
-    plugintools.add_item(action="launch_parsec", title=LANG_TITLE_LAUNCH_PARSEC, thumbnail=DEFAULT_LOGO , fanart=DEFAULT_LOGO , folder=False)
-    plugintools.add_item(action="manage_computers", title=LANG_TITLE_MANAGE_COMPUTERS, thumbnail=DEFAULT_LOGO , fanart=DEFAULT_LOGO , folder=True)
+    # List of available computers
+    global current_computer
+    global parsec_session_id
+
+    plugintools.set_view(plugintools.LIST)
+
+    computers = get_computers()
+    user = get_user_info()
+
+    for index, computer in enumerate(computers):
+        current_computer = computer
+        numberselect = int(index + 1)
+        computer_title = get_computer_title()
+        context=get_computer_context_menu(computer, numberselect)
+        plugintools.log("parsec computer title is " + computer_title)
+        computer_status_logo = get_computer_status_logo()
+
+        plugintools.add_computer_list_item(
+            action="manage_computer",
+            title=computer_title,
+            session_id=parsec_session_id,
+            computer=json.dumps(current_computer),
+            user=json.dumps(user),
+            numberselect=numberselect,
+            thumbnail=computer_status_logo,
+            fanart=DEFAULT_FANART,
+            folder=True,
+            context=context
+        )
 
 
-def launch_parsec(params):
+def get_computer_context_menu(computer, numberselect):
+    """
+    returns a list of context entries per computer
+
+    :param computer:
+    :param numberselect:
+    :return:
+    """
+
+    global parsec_session_id
+
+    switch_computer_action = get_target_state_action()
+    context_label_switch_computer = get_target_title()
+    context_url_switch_computer = '%s?action=%s&title=%s&session_id=%s&computer=%s&thumbnail=%s&fanart=%s&numberselect=%s' % (
+        sys.argv[0],
+        switch_computer_action,
+        urllib.quote_plus(context_label_switch_computer),
+        urllib.quote_plus(parsec_session_id),
+        urllib.quote_plus(json.dumps(computer)),
+        urllib.quote_plus(DEFAULT_LOGO),
+        urllib.quote_plus(DEFAULT_FANART),
+        numberselect,
+    )
+
+    connect_action ='connect_to_computer'
+    context_label_connect_computer = LANG_TITLE_CONNECT_PARSEC
+    context_url_connect_computer = '%s?action=%s&title=%s&session_id=%s&computer=%s&thumbnail=%s&fanart=%s&numberselect=%s' % (
+        sys.argv[0],
+        connect_action,
+        urllib.quote_plus(context_label_connect_computer),
+        urllib.quote_plus(parsec_session_id),
+        urllib.quote_plus(json.dumps(computer)),
+        urllib.quote_plus(DEFAULT_LOGO),
+        urllib.quote_plus(DEFAULT_FANART),
+        numberselect,
+    )
+
+    context_menu = []
+    context_menu.append({'label': context_label_switch_computer, 'url': context_url_switch_computer})
+    context_menu.append({'label': context_label_connect_computer, 'url': context_url_connect_computer})
+
+    return context_menu
+
+
+def connect_to_computer(params):
+    # initializes connection to a running computer
+    global current_computer
+    global parsec_session_id
+    computer_json = params.get('computer')
+    parsec_session_id = params.get('session_id')
+    current_computer = json.loads(computer_json)
+    numberselect = params.get('numberselect')
+
     instance_running = get_is_instance_running()
+
     if instance_running:
         parsec_user = plugintools.get_setting("parsec_user")
         parsec_passwd = plugintools.get_setting("parsec_passwd")
         plugintools.log("parsec checking existance of credentials")
         if parsec_user != "" and parsec_passwd != "":
-            full_command = "/home/osmc/Scripts/parsec-start/xstart.sh " + parsec_user +  " " + parsec_passwd
+            full_command = "/home/osmc/Scripts/parsec-start/xstart.sh " + parsec_user + " " + parsec_passwd + " " + numberselect
             output=os.popen(full_command).read()
             #dialog.ok("Starting X11",output)
             #print output
@@ -89,63 +193,78 @@ def launch_parsec(params):
         plugintools.message("parsec", "Please add your parsec credentials in addon settings")
 
 
-def manage_computers(params):
-    # List of available computers
-    global current_computer
-    global parsec_session_id
-    plugintools.set_view(plugintools.THUMBNAIL)
-    computers = get_computers()
-
-    for computer in computers:
-        current_computer = computer
-        computer_title = get_computer_title()
-        plugintools.log("parsec computer title is " + computer_title)
-        computer_status_logo = get_computer_status_logo()
-        plugintools.add_item(
-            action="manage_computer",
-            title=computer_title,
-            thumbnail=computer_status_logo,
-            fanart=DEFAULT_FANART,
-            extra=json.dumps(current_computer),
-            session_id=parsec_session_id,
-            folder=True
-        )
-
-
 def manage_computer(params):
     # Lists actions for computer
     global current_computer
     global parsec_session_id
-    computer_json = params.get('extra')
+
+    computer_json = params.get('computer')
     parsec_session_id = params.get('session_id')
     current_computer = json.loads(computer_json)
+    numberselect = params.get('numberselect')
+    user_json = params.get('user')
 
     target_state_action = get_target_state_action()
     target_state_title = get_target_title()
     computer_status_logo = get_computer_status_logo()
-    plugintools.add_item(
+
+    plugintools.add_computer_list_item(
         action=target_state_action,
         title=target_state_title,
+        session_id=parsec_session_id,
+        computer=computer_json,
+        user=user_json,
+        numberselect=numberselect,
         thumbnail=computer_status_logo,
         fanart=DEFAULT_FANART,
-        extra=computer_json,
-        session_id=parsec_session_id,
-        folder=False
+        folder=False,
+        context=False
     )
+
+    plugintools.add_computer_list_item(
+        action='connect_to_computer',
+        title=LANG_TITLE_CONNECT_PARSEC,
+        session_id=parsec_session_id,
+        computer=computer_json,
+        user=user_json,
+        numberselect=numberselect,
+        thumbnail=LOGO_SERVER_CONNECT,
+        fanart=DEFAULT_FANART,
+        folder=False,
+        context=False
+    )
+
+
+def redirect_to_main_list(params):
+    # redirecting user to main list
+    global parsec_session_id
+
+    redirect_url = '%s?action=%s&title=%s&session_id=%s&computer=%s&thumbnail=%s&fanart=%s&numberselect=%s' % (
+        sys.argv[0],
+        'main_list',
+        urllib.quote_plus(params.get('title')),
+        urllib.quote_plus(parsec_session_id),
+        urllib.quote_plus(params.get('computer')),
+        urllib.quote_plus(params.get('thumbnail')),
+        urllib.quote_plus(params.get('fanart')),
+        urllib.quote_plus(params.get('numberselect')),
+    )
+
+    xbmc.executebuiltin("Container.Update(%s)" % redirect_url)
 
 
 def switch_computer_on(params):
     # trigger switching on the computer
     global current_computer
     global parsec_session_id
-    computer_json = params.get('extra')
+    computer_json = params.get('computer')
     parsec_session_id = params.get('session_id')
     current_computer = json.loads(computer_json)
     computer_id = current_computer['lease']
     switch_computer_state('on', computer_id)
-    xbmc.executebuiltin(
-        'Notification('+LANG_TITLE_MANAGE_COMPUTER_MESSAGE_ON+',5000,'+DEFAULT_LOGO+')'
-    )
+
+    trigger_notification(LANG_TITLE_MANAGE_COMPUTER_MESSAGE_ON)
+    redirect_to_main_list(params)
     pass
 
 
@@ -153,22 +272,19 @@ def switch_computer_off(params):
     # trigger switching off the computer
     global current_computer
     global parsec_session_id
-    computer_json = params.get('extra')
+    computer_json = params.get('computer')
     parsec_session_id = params.get('session_id')
     current_computer = json.loads(computer_json)
     computer_id = current_computer['lease']
     switch_computer_state('off', computer_id)
-    xbmc.executebuiltin(
-        'Notification('+LANG_TITLE_MANAGE_COMPUTER_MESSAGE_OFF+',5000,'+DEFAULT_LOGO+')'
-    )
+
+    trigger_notification(LANG_TITLE_MANAGE_COMPUTER_MESSAGE_OFF)
+    redirect_to_main_list(params)
     pass
 
 
 def switch_computer_pending(params):
-    # just pass this. working is already in progress
-    xbmc.executebuiltin(
-        'Notification('+LANG_TITLE_MANAGE_COMPUTER_MESSAGE_PENDING+',5000,'+DEFAULT_LOGO+')'
-    )
+    trigger_notification(LANG_TITLE_MANAGE_COMPUTER_MESSAGE_PENDING)
     pass
 
 
@@ -181,10 +297,10 @@ def get_computer_title():
 
 
 def get_computer_status_logo():
+    # Return matching title to current state
     global current_computer
     computer = current_computer
 
-    # Return matching title to current state
     if computer['status'] == 'off':
         status_logo = LOGO_SERVER_OFF
     elif computer['status'] == 'on':
@@ -211,6 +327,21 @@ def get_target_state_action():
     return target_state_action
 
 
+def get_target_state_script():
+    global current_computer
+    computer = current_computer
+
+    # Returns action method string to be target
+    if computer['status'] == 'off':
+        target_state_script = 'computer_start.py'
+    elif computer['status'] == 'on':
+        target_state_script = 'computer_stop.py'
+    else:
+        target_state_script += 'notify_pending.py'
+
+    return target_state_script
+
+
 def get_target_title():
     # Return matching title to current state
     global current_computer
@@ -227,29 +358,41 @@ def get_target_title():
 
 
 def get_computers():
-    # Returns list wit available computers and their metadata
+    # Returns list of available computers and their metadata
     session_id = get_parsec_session_id()
-    plugintools.log("Received parsec sessionid:" + session_id)
-    computers = get_computers_by_session(session_id)
+    plugintools.log("Received parsec sessionid for getting list of computers:" + session_id)
+    computers = get_parsec_request_result(session_id, API_LIST_COMPUTERS)
 
     return computers
 
 
-def get_computers_by_session(session_id):
-    # Using session id for retrievin list of machines
-    user_agent = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:62.0)'
+def get_user_info():
+    # Returns list of user information
+    session_id = get_parsec_session_id()
+    plugintools.log("Received parsec sessionid for fetching user:" + session_id)
+    user = get_parsec_request_result(session_id, API_USER_INFO)
+    return user
+
+
+def get_parsec_request_result(session_id, url):
+    '''
+    requesting parsec by providing session and url
+
+    :param session_id:
+    :param url:
+    :return list:
+    '''
+
     values = {}
-    headers = {
+    header = {
         'User-Agent': user_agent,
         'x-parsec-session-id': session_id
     }
 
-    data = urllib.urlencode(values)
-    req = urllib2.Request(API_LIST_COMPUTERS, data, headers)
-    response = urllib2.urlopen(req)
-    computers = json.load(response)
+    response = requests.get(url, params=values, headers=header)
+    result = response.json()
 
-    return computers
+    return result
 
 
 def get_parsec_session_id():
@@ -260,7 +403,6 @@ def get_parsec_session_id():
         parsec_user = plugintools.get_setting("parsec_user")
         parsec_passwd = plugintools.get_setting("parsec_passwd")
 
-        user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
         values = {'email': parsec_user,
                   'password': parsec_passwd,
                   'expiration_type': 'short'}
@@ -273,6 +415,7 @@ def get_parsec_session_id():
         data = json.load(response)
         parsec_session_id = data['session_id']
 
+    plugintools.log("Received parsec sessionid:" + parsec_session_id)
     return parsec_session_id
 
 
@@ -282,7 +425,6 @@ def switch_computer_state(target_state, computer_id):
     else:
         url = API_SWITCH_COMPUTER_OFF
 
-    user_agent = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:62.0)'
     values = {
         'id': computer_id
     }
@@ -298,6 +440,8 @@ def switch_computer_state(target_state, computer_id):
 
 
 def get_is_instance_running():
+    session_id = get_parsec_session_id()
+
     return True
 
 
--- addon/plugin.program.parsec-launcher/plugintools.py
+++ addon/plugin.program.parsec-launcher/plugintools.py
@@ -53,7 +53,7 @@ from StringIO import StringIO
 import gzip
 import pprint
 import base64
-
+import json
 
 module_log_enabled = False
 http_debug_log_enabled = False
@@ -583,10 +583,79 @@ def add_item( action="" , title="" , plot="" , url="" , thumbnail="" , fanart=""
         itemurl = '%s?action=%s&title=%s&url=%s&thumbnail=%s&plot=%s&extra=%s&page=%s&quality=%s&imdb_rating=%s&comments=%s&movie_language=%s&genre=%s&actorsandmore=%s&landyear=%s&age_recomm=%s&session_id=%s' % ( sys.argv[ 0 ] , action , urllib.quote_plus( title ) , urllib.quote_plus(url) , urllib.quote_plus( thumbnail ) , urllib.quote_plus( plot ) , urllib.quote_plus( extra ) , urllib.quote_plus( page ), urllib.quote_plus( quality ), urllib.quote_plus( imdb_rating ), urllib.quote_plus( comments ), urllib.quote_plus( movie_language ), urllib.quote_plus( genre ), urllib.quote_plus( actorsandmore ), urllib.quote_plus( landyear ), urllib.quote_plus( age_recomm ), urllib.quote_plus( session_id ))
         xbmcplugin.addDirectoryItem( handle=int(sys.argv[1]), url=itemurl, listitem=listitem, isFolder=folder)
 
+
+def add_computer_list_item(action="", title="", thumbnail="", fanart="", session_id="", numberselect="", computer=None, user=None, info_labels=None, folder=True, context=False):
+    listitem = xbmcgui.ListItem(title, iconImage="DefaultVideo.png", thumbnailImage=thumbnail)
+
+    if info_labels is None:
+        info_labels = {"Title": title, "Plot": get_computer_info(computer,user)}
+
+    listitem.setInfo("video", info_labels)
+
+    if fanart != "":
+        listitem.setProperty('fanart_image', fanart)
+        xbmcplugin.setPluginFanart(int(sys.argv[1]), fanart)
+
+    if context:
+        context_entries = []
+
+        for context_entry in context:
+            context_label = context_entry.get('label')
+            context_url = context_entry.get('url')
+            runner = "XBMC.RunPlugin(" + str(context_url) + ", "")"
+            context_entries.append((str(context_label), runner,))
+
+        listitem.addContextMenuItems(context_entries)
+
+
+    itemurl = '%s?action=%s&title=%s&session_id=%s&computer=%s&thumbnail=%s&fanart=%s&numberselect=%s' % (
+        sys.argv[0],
+        action,
+        urllib.quote_plus(title),
+        urllib.quote_plus(session_id),
+        urllib.quote_plus(computer),
+        urllib.quote_plus(thumbnail),
+        urllib.quote_plus(fanart),
+        numberselect,
+    )
+    xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=itemurl, listitem=listitem, isFolder=folder)
+
+
+def get_computer_info(computer_json, user_json):
+    computer = json.loads(computer_json)
+    user = json.loads(user_json)
+    addon=__settings__
+
+    LANG_TITLE_STATUS = addon.getLocalizedString(30027).encode('utf-8')
+    LANG_TITLE_CREATED = addon.getLocalizedString(30028).encode('utf-8')
+    LANG_TITLE_LAST_UPDATED = addon.getLocalizedString(30029).encode('utf-8')
+    LANG_TITLE_PROVIDER = addon.getLocalizedString(30030).encode('utf-8')
+    LANG_TITLE_MACHINE_TYPE = addon.getLocalizedString(30031).encode('utf-8')
+    LANG_TITLE_REGION = addon.getLocalizedString(30032).encode('utf-8')
+    LANG_TITLE_CREDIT = addon.getLocalizedString(30033).encode('utf-8')
+    LANG_TITLE_PLAYTIME = addon.getLocalizedString(30034).encode('utf-8')
+
+    credits_dollar = str(user['credits']/100)
+    play_time = str(user['play_time']/60/60)
+
+    computer_info = ""
+    computer_info += LANG_TITLE_STATUS + ": " + computer['status'] + "\n"
+    computer_info += LANG_TITLE_CREATED + ": " + computer['created_at'] + "\n"
+    computer_info += LANG_TITLE_LAST_UPDATED + ": " + computer['updated_at'] + "\n"
+    computer_info += LANG_TITLE_PROVIDER + ": " + computer['managed']['provider_name'] + "\n"
+    computer_info += LANG_TITLE_MACHINE_TYPE + ": " + computer['managed']['machine_type'] + "\n"
+    computer_info += LANG_TITLE_REGION + ": " + computer['managed']['region'] + "\n"
+    computer_info += LANG_TITLE_CREDIT + ": " + credits_dollar + "\n"
+    computer_info += LANG_TITLE_PLAYTIME + ": " + play_time + "\n"
+
+    return computer_info
+
+
 def close_item_list():
     _log("close_item_list")
 
     xbmcplugin.endOfDirectory(handle=int(sys.argv[1]), succeeded=True)
+
 
 def play_resolved_url(url):
     _log("play_resolved_url ["+url+"]")
@@ -594,6 +663,7 @@ def play_resolved_url(url):
     listitem = xbmcgui.ListItem(path=url)
     listitem.setProperty('IsPlayable', 'true')
     return xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, listitem)
+
 
 def direct_play(url):
     _log("direct_play ["+url+"]")
